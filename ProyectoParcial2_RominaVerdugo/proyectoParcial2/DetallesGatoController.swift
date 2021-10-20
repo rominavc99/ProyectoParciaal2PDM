a5540af598877bad7e1f769444430b519d728030
rominavc99 <53828115+rominavc99@users.noreply.github.com>
2021-10-20 13:24:58 -0700

    Add files via upload

---
+++ ProyectoParcial2_RominaVerdugo/proyectoParcial2/DetallesGatoController.swift
@@ -0,0 +1,56 @@
+//
+//  DetallesGatoController.swift
+//  proyectoParcial2
+//
+//  Created by Blanca Castellanos  on 14/10/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetallesGatoController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+   
+    @IBOutlet weak var tvCualidades: UITableView!
+    var cualidades : [Cualidad] = []
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cualidades.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celdaDetalles = tableView.dequeueReusableCell(withIdentifier: "CeldaDetallesGato") as! CeldaDetallesGatoController
+        
+        celdaDetalles.lblCualidad.text = cualidades[indexPath.row].gatoCualidad
+        celdaDetalles.fotoCualidad.image = UIImage(named: cualidades[indexPath.row].iconCualidad!)
+        
+        return celdaDetalles
+    }
+    
+
+    @IBOutlet weak var lblOrigen: UILabel!
+    @IBOutlet weak var lblEdad: UILabel!
+    @IBOutlet weak var lblCaracter: UILabel!
+    @IBOutlet weak var lblPelaje: UILabel!
+    @IBOutlet weak var lblFotoGato: UIImageView!
+    
+    var gato : Gato = Gato(raza: "", pelaje: "", edadEstimada: "", origen: "", caracter: "")
+    
+    override func viewDidLoad() {
+        
+        self.title = gato.raza
+        
+        
+        lblEdad.text = gato.edadEstimada
+        lblCaracter.text = gato.caracter
+        lblPelaje.text = gato.pelaje
+        
+        
+      
+    }
+}
+
